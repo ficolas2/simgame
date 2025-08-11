0c9a650cc4dd2244ebe006986ab06e3ff478fb61
ficolas2 <nicolasdepg@gmail.com>
2025-08-11 03:51:57 +0200

    feat: 3d renderer

--- rust/src/utils/tile_utils.rs
+++ rust/src/utils/tile_utils.rs
@@ -1,12 +1,13 @@
-use godot::builtin::Vector2;
+use godot::builtin::Vector3;
 
 pub const TILE_SIZE: f32 = 32.0;
 
 pub const TILE_WIDTH: f32 = TILE_SIZE;
 pub const TILE_HEIGHT: f32 = TILE_SIZE / 2.0;
 
-pub fn iso_to_screen(x: i32, y: i32, z: i32) -> Vector2 {
-    let px = (x - y) as f32 * TILE_WIDTH * 0.5;
-    let py = (x + y - z) as f32 * TILE_HEIGHT * 0.5;
-    return Vector2::new(px, py);
+pub fn iso_to_world(x: i32, y: i32, z: i32) -> Vector3 {
+    let px = (x - y) as f32 * 0.5;
+    let py = -(x + y - z) as f32 * 0.25;
+    let order = (x + y) as f32 * 20.0 + z as f32;
+    return Vector3::new(px, py, order);
 }
--- rust/src/world/world_renderer.rs
+++ rust/src/world/world_renderer.rs
@@ -1,33 +1,36 @@
 use godot::{
-    classes::{IMultiMeshInstance2D, MultiMeshInstance2D},
+    classes::{IMultiMeshInstance3D, MultiMeshInstance3D},
     prelude::*,
 };
 
-use crate::utils::tile_utils::{TILE_SIZE, iso_to_screen};
+use crate::utils::tile_utils::iso_to_world;
 
 use super::world::World;
 
-const MAX_COUNT: i32 = 30000;
+const MAX_COUNT: i32 = 50000;
 
 #[derive(GodotClass)]
-#[class(base=MultiMeshInstance2D)]
+#[class(base=MultiMeshInstance3D)]
 struct WorldRenderer {
     #[export]
     world: Option<Gd<World>>,
 
-    base: Base<MultiMeshInstance2D>,
+    base: Base<MultiMeshInstance3D>,
 
     #[export]
     height: i32,
+    #[export]
+    z_count: i32,
 }
 
 #[godot_api]
-impl IMultiMeshInstance2D for WorldRenderer {
-    fn init(base: Base<MultiMeshInstance2D>) -> Self {
+impl IMultiMeshInstance3D for WorldRenderer {
+    fn init(base: Base<MultiMeshInstance3D>) -> Self {
         Self {
             world: None,
             base,
             height: 10,
+            z_count: 10,
         }
     }
 
@@ -39,39 +42,36 @@ impl IMultiMeshInstance2D for WorldRenderer {
 
     fn process(&mut self, _delta: f64) {
         let height = self.height;
+        let z_count = self.z_count;
         let mut world = self.world.clone();
         let owner = self.base_mut();
-        // Get camera position
-        let viewport = owner.get_viewport().expect("No viewport");
-        let cam_handle = viewport.get_camera_2d().expect("No Camera2D in viewport");
-        let cam_pos = cam_handle.get_global_position();
-        let viewport_size =
-            viewport.get_visible_rect().size / cam_handle.get_zoom() / (TILE_SIZE / 2.0);
 
-        // TODO read from world
         let mut world = world.as_mut().unwrap().bind_mut();
         let mut mm = owner.get_multimesh().unwrap();
 
-        let x_count = viewport_size.x as i32 / 2 + 3;
-        let y_count = viewport_size.y as i32 * 2 + 3;
+        let x_count = 50; // TODO hardcoded
+        let y_count = 50;
 
-        let mut transform_array = PackedVector2Array::new();
+        let mut transform_array = PackedVector3Array::new();
         let mut custom_data_array = VariantArray::new();
 
-        let x_axis = Vector2::new(1.0, 0.0);
-        let y_axis = Vector2::new(0.0, 1.0);
-        let zero = Vector2::new(0.0, 0.0);
+        let x_axis = Vector3::new(1.0, 0.0, 0.0);
+        let y_axis = Vector3::new(0.0, 1.0, 0.0);
+        let z_axis = Vector3::new(0.0, 0.0, 1.0);
+        let zero = Vector3::new(0.0, 0.0, 0.0);
         // TODO: a few can be removed by only rendering the top ones from
         let zero_color = Color::from_rgba(0.0, 0.0, 0.0, 0.0);
         // the furthest y
-        let z_count = 10;
 
+
+        let mut i = 0;
         for row in 0..y_count + z_count {
             let start_x = (row + 1) / 2 - x_count;
             let start_y = row / 2 + 1;
 
             for column in 0..x_count {
-                for z in (height - z_count)..=height {
+                for z in (height-z_count)..=height {
+                // for z in (height - z_count)..=height {
                     let x = start_x + column;
                     let y = start_y - column;
 
@@ -79,43 +79,39 @@ impl IMultiMeshInstance2D for WorldRenderer {
                     if tile.tile_id == 0 {
                         continue;
                     }
+                    i+=1;
 
                     // Transform
-                    let origin = iso_to_screen(x, y, z);
+                    let origin = iso_to_world(x, y, z);
                     // mm.set_instance_transform_2d(i, xf);
                     transform_array.push(x_axis);
                     transform_array.push(y_axis);
+                    transform_array.push(z_axis);
                     transform_array.push(origin);
 
                     // UVs
                     let u = ((tile.tile_id - 1) % 11) as f32;
                     let v = ((tile.tile_id - 1) / 11) as f32;
 
-                    // let custom_data = get_custom_data(u, v, world.get_tile(x, y, z + 1).tile_id == 0);
-                    let darkened = if world.get_tile(x, y, z + 1).tile_id == 0 {
-                        0.0
-                    } else {
-                        1.0
-                    };
-
-                    let custom_data = Color::from_rgba(u/11.0, v/11.0, darkened, 1.0);
+                    let custom_data = Color::from_rgba(u/11.0, v/11.0, 0.0, 1.0);
                     custom_data_array.push(&custom_data.to_variant());
                 }
             }
         }
 
-        let visible_count = (transform_array.len() / 3) as i32;
+        let visible_count = (transform_array.len() / 4) as i32;
         let max_count = MAX_COUNT;
         for _ in visible_count..max_count {
             transform_array.push(x_axis);
             transform_array.push(y_axis);
+            transform_array.push(z_axis);
             transform_array.push(zero);
 
             custom_data_array.push(&zero_color.to_variant());
         }
 
         mm.set_visible_instance_count(visible_count);
-        mm.set("transform_2d_array", &Variant::from(transform_array));
+        mm.set("transform_array", &Variant::from(transform_array));
         mm.set("custom_data_array", &Variant::from(custom_data_array));
     }
 }
